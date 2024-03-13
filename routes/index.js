f2ac8b6fe701e8a233477fec7946331f92f5c77a
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-03-13 15:44:35 +0300

    3. Create a new user -- updated the index.js with the new endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 // Define routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// Users routes
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
