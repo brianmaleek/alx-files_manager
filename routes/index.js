f245322e52ea270b4e16dacf674cce6732861135
washucode <estherw8525@gmail.com>
2024-03-13 22:52:05 +0300

    add all file routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-
+import FilesController from '../controllers/FilesController';
 const router = express.Router();
 
 // Define routes
@@ -11,4 +11,20 @@ router.get('/stats', AppController.getStats);
 // Users routes
 router.post('/users', UsersController.postNew);
 
+//file upload
+router.post('/files', FilesController.postUpload);
+
+// get file
+router.get('/files/:id', FilesController.getShow);
+
+// put file
+router.put('/files/:id/publish', FilesController.putPublish);
+
+// put file unpublish
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+// post data
+router.post('/files/:id/data', FilesController.getFile);
+
 module.exports = router;
+
